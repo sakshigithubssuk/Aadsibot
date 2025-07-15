5a3b00b7f9aede2f611422fd5fedd10f9f2a1217
sakshigithubssuk <165806049+sakshigithubssuk@users.noreply.github.com>
2025-07-15 19:02:46 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   whatsappNumber: { type: String, required: true},
-  credits: { type: Number, default: 0 }, // Free credits
+  credits: { type: Number, default: 10 }, // Free credits
 
   // --- NEW FIELDS REQUIRED FOR TELEGRAM BOT ---
 
