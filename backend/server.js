f196ba25dfff479f39435b3497c99bb29e9138b6
sakshigithubssuk <165806049+sakshigithubssuk@users.noreply.github.com>
2025-07-19 15:05:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const app = express();
 // --- 4. MIDDLEWARE CONFIGURATION ---
 // CORS
 app.use(cors({
-  origin: 'http://localhost:5173', // Your React app's URL
+  origin:  'https://aadsibot.vercel.app', // Your React app's URL
   credentials: true,
 }));
 
